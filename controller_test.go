5ecc9ba19744bf856bb23754faef5c5c14e78e96
Tristan Colgate <tcolgate@gmail.com>
2022-07-14 10:44:40 +0100

    testing: start on controller test

--- controller_test.go
+++ controller_test.go
@@ -1,7 +1,260 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	workflow "github.com/argoproj/argo-workflows/v3/pkg/apis/workflow/v1alpha1"
+	workflowfake "github.com/argoproj/argo-workflows/v3/pkg/client/clientset/versioned/fake"
+	informers "github.com/argoproj/argo-workflows/v3/pkg/client/informers/externalversions"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	k8sinformers "k8s.io/client-go/informers"
+	k8sfake "k8s.io/client-go/kubernetes/fake"
+	k8stesting "k8s.io/client-go/testing"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/utils/diff"
+)
+
+type fakeStorageManager struct {
+}
+
+func (f *fakeStorageManager) ensurePVC(wf *workflow.Workflow, org, repo, branch string, defaults CacheSpec) error {
+	panic("not implemented")
+}
+
+func (f *fakeStorageManager) deletePVC(org, repo, branch string, action string) error {
+	panic("not implemented")
+}
 
 func TestWFName(t *testing.T) {
 	t.Logf("name: %q", wfName("ci", "qubitdigital", "yak", "mytests/tester"))
 }
+
+type fixture struct {
+	t *testing.T
+
+	client     *workflowfake.Clientset
+	kubeclient *k8sfake.Clientset
+
+	// Objects to put in the store.
+	workflowsLister []*workflow.Workflow
+	// Actions expected to happen on the client.
+	kubeactions []k8stesting.Action
+	actions     []k8stesting.Action
+	// Objects from here preloaded into NewSimpleFake.
+	kubeobjects []runtime.Object
+	objects     []runtime.Object
+}
+
+func newFixture(t *testing.T) *fixture {
+	f := &fixture{}
+	f.t = t
+	f.objects = []runtime.Object{}
+	f.kubeobjects = []runtime.Object{}
+	return f
+}
+
+var (
+	alwaysReady        = func() bool { return true }
+	noResyncPeriodFunc = func() time.Duration { return 0 }
+)
+
+func (f *fixture) newController() (*workflowSyncer, informers.SharedInformerFactory, k8sinformers.SharedInformerFactory) {
+	f.client = workflowfake.NewSimpleClientset(f.objects...)
+	f.kubeclient = k8sfake.NewSimpleClientset(f.kubeobjects...)
+
+	i := informers.NewSharedInformerFactory(f.client, noResyncPeriodFunc())
+	k8sI := k8sinformers.NewSharedInformerFactory(f.kubeclient, noResyncPeriodFunc())
+
+	config := Config{}
+	storage := &fakeStorageManager{}
+	clients := &testGHClientSrc{}
+
+	c := newWorkflowSyncer(
+		f.kubeclient,
+		f.client,
+		i,
+		storage,
+		clients,
+		1234,
+		[]byte("secret"),
+		"http://example.com/ui",
+		config,
+	)
+
+	/*
+		for _, f := range f.workflowLister {
+			i.Config().V1beta1().RuleGroups().Informer().GetIndexer().Add(f)
+		}
+	*/
+
+	return c, i, k8sI
+}
+
+func (f *fixture) run(obj interface{}, t *testing.T) {
+	f.runController(obj, true, false, t)
+}
+
+func (f *fixture) runExpectError(obj interface{}, t *testing.T) {
+	f.runController(obj, true, true, t)
+}
+
+func (f *fixture) runController(obj interface{}, startInformers bool, expectError bool, t *testing.T) {
+	c, i, k8sI := f.newController()
+	if startInformers {
+		stopCh := make(chan struct{})
+		defer close(stopCh)
+		i.Start(stopCh)
+		k8sI.Start(stopCh)
+	}
+
+	switch obj := obj.(type) {
+	case *workflow.Workflow:
+		err := c.sync(obj)
+		if !expectError && err != nil {
+			f.t.Errorf("error syncing workflow: %v", err)
+		} else if expectError && err == nil {
+			f.t.Error("expected error syncing workflow, got nil")
+		}
+	default:
+	}
+
+	actions := filterInformerActions(f.client.Actions())
+	f.t.Logf("actions: %#v", actions)
+	for i, action := range actions {
+		if len(f.actions) < i+1 {
+			f.t.Errorf("%d unexpected actions: %#v", len(actions)-len(f.actions), actions[i:])
+			break
+		}
+
+		expectedAction := f.actions[i]
+		checkAction(expectedAction, action, f.t)
+	}
+
+	if len(f.actions) > len(actions) {
+		f.t.Errorf("%d additional expected actions:%+v", len(f.actions)-len(actions), f.actions[len(actions):])
+	}
+
+	k8sActions := filterInformerActions(f.kubeclient.Actions())
+	f.t.Logf("k8s actions: %#v", k8sActions)
+	for i, action := range k8sActions {
+		if len(f.kubeactions) < i+1 {
+			f.t.Errorf("%d unexpected k8s actions: %+v", len(k8sActions)-len(f.kubeactions), k8sActions[i:])
+			break
+		}
+
+		expectedAction := f.kubeactions[i]
+		checkAction(expectedAction, action, f.t)
+	}
+
+	if len(f.kubeactions) > len(k8sActions) {
+		f.t.Errorf("%d additional expected k8s actions:%+v", len(f.kubeactions)-len(k8sActions), f.kubeactions[len(k8sActions):])
+	}
+}
+
+// checkAction verifies that expected and actual actions are equal and both have
+// same attached resources
+func checkAction(expected, actual k8stesting.Action, t *testing.T) {
+	if !(expected.Matches(actual.GetVerb(), actual.GetResource().Resource) && actual.GetSubresource() == expected.GetSubresource()) {
+		t.Errorf("Expected\n\t%#v\ngot\n\t%#v", expected, actual)
+		return
+	}
+
+	if reflect.TypeOf(actual) != reflect.TypeOf(expected) {
+		t.Errorf("Action has wrong type. Expected: %t. Got: %t", expected, actual)
+		return
+	}
+
+	switch a := actual.(type) {
+	case k8stesting.CreateAction:
+		e, _ := expected.(k8stesting.CreateAction)
+		expObject := e.GetObject()
+		object := a.GetObject()
+
+		if !reflect.DeepEqual(expObject, object) {
+			t.Errorf("Action %s %s has wrong object\nDiff:\n %s",
+				a.GetVerb(), a.GetResource().Resource, diff.ObjectGoPrintDiff(expObject, object))
+		}
+	case k8stesting.UpdateAction:
+		e, _ := expected.(k8stesting.UpdateAction)
+		expObject := e.GetObject()
+		object := a.GetObject()
+
+		if !reflect.DeepEqual(expObject, object) {
+			t.Errorf("Action %s %s has wrong object\nDiff:\n %s",
+				a.GetVerb(), a.GetResource().Resource, diff.ObjectGoPrintDiff(expObject, object))
+		}
+	}
+}
+
+// filterInformerActions filters list and watch actions for testing resources.
+// Since list and watch don't change resource state we can filter it to lower
+// nose level in our tests.
+func filterInformerActions(actions []k8stesting.Action) []k8stesting.Action {
+	ret := []k8stesting.Action{}
+	for _, action := range actions {
+		if action.Matches("get", "workflows") ||
+			action.Matches("list", "workflows") ||
+			action.Matches("watch", "workflows") {
+			continue
+		}
+		ret = append(ret, action)
+	}
+
+	return ret
+}
+
+func (f *fixture) expectCreateWorkflowAction(rs *workflow.Workflow) {
+	f.actions = append(f.actions,
+		k8stesting.NewCreateAction(schema.GroupVersionResource{
+			Resource: "workflows",
+			Group:    workflow.SchemeGroupVersion.Group,
+			Version:  workflow.SchemeGroupVersion.Version,
+		}, rs.Namespace, rs),
+	)
+}
+
+func (f *fixture) expectUpdateWorkflowsAction(rs *workflow.Workflow) {
+	f.actions = append(f.actions, k8stesting.NewUpdateAction(schema.GroupVersionResource{
+		Resource: "workflows",
+		Group:    workflow.SchemeGroupVersion.Group,
+		Version:  workflow.SchemeGroupVersion.Version,
+	}, rs.Namespace, rs))
+}
+
+func getKey(obj interface{}, t *testing.T) string {
+	key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+	if err != nil {
+		t.Errorf("Unexpected error getting key for %v: %v", obj, err)
+		return ""
+	}
+	return key
+}
+
+/*
+func TestCreatesRuleGroup(t *testing.T) {
+	f := newFixture(t)
+	rs := newRuleGroup("test", testGroup)
+	rs.Status.RecordingRuleCount = 2
+
+	f.ruleGroupLister = append(f.ruleGroupLister, rs)
+	f.objects = append(f.objects, rs)
+	//f.kubeobjects = append(f.kubeobjects, cm)
+
+	ucm := newConfigMap(
+		"default",
+		"prom-config-controller",
+		"prom-config-controller.yaml",
+		testConfigMap)
+	cm := ucm.DeepCopy()
+	cm.Data = map[string]string{}
+
+	f.expectCreateConfigMapAction(cm)
+	f.expectUpdateConfigMapAction(ucm)
+	//f.expectUpdateRuleGroupsStatusAction(nrs)
+
+	f.run(rs, t)
+}
+*/
